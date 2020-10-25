dc90c7b723ad6d87ee7c32db9ce1d05086acff1b
Guilherme Pereira <up201809622@fc.up.pt>
2020-10-25 11:43:00 +0000

    finished flip function

--- index.js
+++ index.js
@@ -6,37 +6,52 @@ onButtonInstructionsClick = function(oEvent) {
   instructionsDiv.hidden = !instructionsDiv.hidden;
 }
 
-const counter_p1 = document.getElementById("scr_p1");
-const counter_p2 = document.getElementById("scr_p2");
+const counter_p1 = document.getElementById("scr_p1");     // Score player 1
+const counter_p2 = document.getElementById("scr_p2");     // Score player 2
+//let end_p1 = false;                                       // Player 1 cant play if flag==true
+//let end_p2 = false;                                       //  ||    2  ||   ||  ||  ||    ||    => If both true , game over!
+let player_counter = 1;                                   // Player counter to control game flow
+
+let curr_player_dot = "dotp1";
 
 window.onload = function() {
   let candidate_moves = new Array(64);
   const gameboard = new Reversi("base");
-  const pl_1 = new Player("black");
-  const pl_2 = new Player("white");
-  counter_p1.innerHTML = 2; counter_p2.innerHTML = 2;     //  init score
-  validate_position(pl_1.ply_color,gameboard.data_dots,candidate_moves);
+  counter_p1.innerHTML = 2;                  //  init score
+  counter_p2.innerHTML = 2;
 
-  let curr_player = pl_1;
-  let type_dot;
-  if (curr_player == pl_1) {
-    type_dot = "dotp1";
-  } else {
-    type_dot = "dotp2";
-  }
+  validate_position(curr_player_dot,gameboard.data_dots,candidate_moves);
+
   let cells = document.getElementsByClassName("cell");
   let candidate_dots  = document.getElementsByClassName("dotplace");
 
-    for (let i = 0; i < candidate_dots.length; i++) {
-      candidate_dots[i].onclick = function() {
-        let cell = candidate_dots[i].parentElement;
+    for (let i = 0; i < cells.length; i++) {
+      cells[i].onclick = function() {
+        let index = -1;
+        for (let j = 0; j < candidate_dots.length; j++) {
+          if (candidate_dots[j].parentElement.id == cells[i].id) {
+            index = j;
+            break;
+          }
+        }
+        // debugger;
+        if (index >= 0) {
+          let cell = candidate_dots[index].parentElement;
 
-        let arr_dots = get_array_dots(cells);
-        let index_dot = Array.prototype.slice.call(cells).indexOf(cell);
-        flip_enemy(gameboard.data_dots, index_dot, type_dot);
+          let arr_dots = get_array_dots(cells);
+          let index_dot = Array.prototype.slice.call(cells).indexOf(cell);
+          flip_enemy(gameboard.data_dots, index_dot, curr_player_dot);
+          clear_board(candidate_dots);
+          player_counter++;
+
+          player_counter % 2 == 0 ? curr_player_dot="dotp2" : curr_player_dot="dotp1"
+          validate_position(curr_player_dot,gameboard.data_dots,candidate_moves);
+          // if (end_p1 && end_p2) { // end game
+          // return;
+          // }
+        }
       }
     }
-
 }
 
 function get_array_dots(cells) {
@@ -50,6 +65,14 @@ function get_array_dots(cells) {
     }
   }
   return arr_dots;
+}
+
+function clear_board(gray_dots) {
+  for (let i = 0; i < gray_dots.length; i++) {
+    gray_dots[i].classList.remove("dotplace");
+  }
+  gray_dots[0].classList.remove("dotplace");      // (???)
+  gray_dots.clear;
 }
 
 class Player {
@@ -81,6 +104,7 @@ class Reversi {
       let dot   = document.createElement("div");
 
       cell.className  = "cell";
+      cell.id = i;
       board.appendChild(cell);
       if (i==27 || i==36) {
         dot.className = "dotp1";
@@ -90,9 +114,9 @@ class Reversi {
       cell.appendChild(dot);
 
 
-      cell.onclick = ((fun,pos) => {    // Função anonima que é criado 64 vezes
-              return () => fun(pos);
-      })(fn,i);
+      // cell.onclick = ((fun,pos) => {    // Função anonima que é criado 64 vezes
+      //         return () => fun(pos);
+      // })(fn,i);
 
 
       this.cells[i] = cell;
@@ -114,9 +138,9 @@ class Reversi {
 
 /*-------------------------------------------------------------------------*/
 
-function validate_position(color_pl,board,candidate_moves) {
-  let enemy,friendly;
-  color_pl == "black" ? (enemy="dotp2",friendly="dotp1") : (enemy="dotp1",friendly="dotp2")
+function validate_position(friendly,board,candidate_moves) {
+  let enemy;
+  friendly == "dotp1" ? (enemy="dotp2") : (enemy="dotp1")
 
   for (let pos = 0; pos < 64; pos++) {
 
@@ -127,13 +151,17 @@ function validate_position(color_pl,board,candidate_moves) {
         let adj_pos_lower = pos+i;
         if (adj_pos_upper >= 0 && board[adj_pos_upper].className == enemy && valid_pos_upper(board,friendly,adj_pos_upper,i) == true) {
           let dot = board[pos];
-          dot.className = "dotplace";
-          candidate_moves.push(pos);
+          if (dot.className != "dotp1" && dot.className != "dotp2") {
+            dot.className = "dotplace";
+            candidate_moves.push(pos);
+          }
         }
         if (adj_pos_lower < 64 && board[adj_pos_lower].className == enemy && valid_pos_lower(board,friendly,adj_pos_lower,i) == true) {
           let dot = board[pos];
-          dot.className = "dotplace";
-          candidate_moves.push(pos);
+          if (dot.className != "dotp1" && dot.className != "dotp2") {
+            dot.className = "dotplace";
+            candidate_moves.push(pos);
+          }
         }
       }
 
@@ -141,16 +169,26 @@ function validate_position(color_pl,board,candidate_moves) {
       let lst_pos = pos-1;
       if (nxt_pos < 64 && board[nxt_pos].className == enemy && valid_pos_nxt(board,friendly,nxt_pos) == true) {
         let dot = board[pos];
-        dot.className = "dotplace";
-        candidate_moves.push(pos);
+        if (dot.className != "dotp1" && dot.className != "dotp2") {
+          dot.className = "dotplace";
+          candidate_moves.push(pos);
+        }
       }
       if (lst_pos >= 0 && board[lst_pos].className == enemy && valid_pos_lst(board,friendly,lst_pos) == true) {
         let dot = board[pos];
-        dot.className = "dotplace";
-        candidate_moves.push(pos);
+        if (dot.className != "dotp1" && dot.className != "dotp2") {
+          dot.className = "dotplace";
+          candidate_moves.push(pos);
+        }
       }
     }
   }
+
+  // if (candidate_moves.length == 0 && friendly == "dotp1") {
+  //   end_p1 = true;
+  // } else if (candidate_moves.length == 0 && friendly == "dotp2") {
+  //   end_p2 = true;
+  // }
 }
 
 function valid_pos_upper(board,friendly,pos,i) {
