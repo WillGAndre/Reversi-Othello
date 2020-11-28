885c0bf3956f14d9ab6bc0eb91e90a41b215427d
Nuno Taveira <up201809591@fc.up.pt>
2020-11-28 19:10:50 +0000

    Service.js with all calls

--- service.js
+++ service.js
@@ -1,27 +1,54 @@
 const othelloService = new Service();
 
-// const dummyUser = {'group': 15, 'nick': "ola", 'pass': "ola"}
-// othelloService.join(dummyUser, callbackTest);
-
 function Service() {
     this.ServerUrl = "http://twserver.alunos.dcc.fc.up.pt:8008/";
     this.APIEndPoints = {
         register: "register",
-        update: "update?nick=__NICKNAME__&game=__GAMEHASH__",
+        update: "update",
         join: "join",
-        notify: "",
-        ranking: "",
-        leave: ""
+        notify: "notify",
+        ranking: "ranking",
+        leave: "leave"
     }
 
-    this.update = async function(data){
+    // GET
+    this.events = null;
+    this.update = async function (data) {
+        let ret = null;
+        
+        this.events = new EventSource(this.ServerUrl + this.APIEndPoints.update + this.encodeQueryParams(data));
+        this.events.onopen = function () {
+            console.log("Connection is open");
+        }
+        this.events.onmessage = function (event) {
+            ret = JSON.parse(event.data);
+        }
+        this.events.onerror = function () {
+            console.log("Error in connection " + this.events.readyState);
+            this.events.close();
+            setInterval(() => {
+                if (this.events.readyState == EventSource.CLOSED) {
+                    this.update(data);
+                }
+            }, 4000);
+        }
+        // this.events.close();       Must close when game ends
+        return ret;
+    }
 
+    // POST
+    this.notify = async function (data) {
+        return await this.post(this.ServerUrl + this.APIEndPoints.notify, data);
+    }
+    this.ranking = async function (data) {
+        return await this.post(this.ServerUrl + this.APIEndPoints.ranking, {});
+    }
+    this.leave = async function (data) {
+        return await this.post(this.ServerUrl + this.APIEndPoints.leave, data);
     }
     this.register = async function (data) {
         return await this.post(this.ServerUrl + this.APIEndPoints.register, data);
     }
-    
-    // let ret = await othelloService.join(data);
     this.join = async function (data) {
         return await this.post(this.ServerUrl + this.APIEndPoints.join, data);
     }
@@ -37,10 +64,17 @@ function Service() {
             return error;
         }
     }
-    this.encodeQueryParams = function(data){
-        if(typeof(data)){
 
+    this.encodeQueryParams = function (data) {
+        let aEntries = Object.entries(data);
+        let ret = "";
+        if (aEntries.length !== 0) {
+            ret = "?";
+            for (const keyValue of aEntries) {
+                ret += keyValue[0] + "=" + encodeURIComponent(keyValue[1]) + "&";
+            }
+            ret = ret.substring(0, ret.length - 2);
         }
-        let ret = "?"
+        return ret;
     }
 }
